c68dc91726f44a031ab89ed962e3421b22941da8
AlexKutschera <alexkutschera01@gmail.com>
2017-09-22 07:55:13 +0200

    save to file

--- src/alexa/Main.java
+++ src/alexa/Main.java
@@ -9,20 +9,26 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main extends Application {
 
     private static Label label_ascii, label_binary, label_hex;
     private static TextField input;
-    private static Button button_encode;
+    private static Button button_store;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        int WIDTH = 286;
+        int WIDTH = 346;
 
         Parent root = FXMLLoader.load(Main.class.getResource("ui.fxml"));
 
         primaryStage.setTitle("ASCII Encoder");
-        primaryStage.setScene(new Scene(root, WIDTH, 124));
+        primaryStage.setScene(new Scene(root, WIDTH, 136));
         primaryStage.setMinWidth(WIDTH);
         primaryStage.setMaxWidth(WIDTH);
         primaryStage.show();
@@ -33,7 +39,12 @@ public class Main extends Application {
 
         input = (TextField) root.lookup("#input_encode");
         input.textProperty().addListener((observable, oldValue, newValue) -> {
-            doEncode(newValue);
+            encode(newValue);
+        });
+
+        button_store = (Button) root.lookup("#button_store");
+        button_store.setOnMouseClicked(event -> {
+            store(input.getText());
         });
 
     }
@@ -43,7 +54,36 @@ public class Main extends Application {
         launch(args);
     }
 
-    private static void doEncode(String input){
+    private static void store(String input){
+        String ascii = "ASCII: ";
+        String binary = "BINÄR: ";
+        String hex = "HEXADEZIMAL: ";
+
+        for (char character:input.toCharArray()) {
+            ascii += (int) character;
+            ascii += " ";
+
+            binary += Integer.toBinaryString(character);
+            binary += " ";
+
+            hex += Integer.toHexString(character);
+            hex += " ";
+        }
+
+        try {
+            DateFormat format = new SimpleDateFormat("ddMMyyyy_HHmmss");
+            PrintWriter writer = new PrintWriter("ascii-encoded_"+format.format(new Date())+".txt");
+            writer.println("ORIGINAL: "+input);
+            writer.println(ascii);
+            writer.println(binary);
+            writer.println(hex);
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void encode(String input){
 
         String ascii = "";
         String binary = "";
